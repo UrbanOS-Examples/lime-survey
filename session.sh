eb374eb16d17ddd9070cb9b34df5e3673789dcde
k.carrabine <kevincarrabine23@gmail.com>
2020-05-29 14:26:28 -0400

    uncommented actual delete of sessions

--- session.sh
+++ session.sh
@@ -2,6 +2,4 @@
 path_to_sessions="/var/lib/php/sessions/"
 sessions_to_delete=$(find ${path_to_sessions} -mmin +60 | wc -l)
 
-# find /var/lib/php/sessions -mmin +60 -delete | echo "[$(date '+%d/%m/%Y %H:%M:%S')] crontab - Deleting ${sessions_to_delete} sessions in ${path_to_sessions}"
-
-echo "Running cron and apache"
+find /var/lib/php/sessions -mmin +60 -delete | echo "[$(date '+%d/%m/%Y %H:%M:%S')] crontab - Deleting ${sessions_to_delete} sessions in ${path_to_sessions}"
